7876f24abdf799995c333bb11ed15b89dff0785c
Nada kamal <120226503+nadakamal5@users.noreply.github.com>
2023-02-12 20:41:34 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# todo-app
+# todo-mobile-app
+Todo app
+# Demo 
+![todo_app_run_desktop](https://user-images.githubusercontent.com/48530021/218273537-0344bde4-b8b0-482e-8e10-2a351cad3704.gif)
---
+++ components/taskItem.jsx
@@ -0,0 +1,69 @@
+import { StyleSheet, View, Text, Button, TouchableOpacity } from "react-native";
+import CheckBox from "expo-checkbox";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
+
+export const TaskItem = (props) => {
+  return (
+    <View style={styles.item}>
+      <CheckBox
+        style={styles.checkbox}
+        color={"black"}
+        value={props.checked}
+        onValueChange={(newValue) => {
+          if (newValue) props.checkTask(props.index);
+          else props.unCheckTask(props.index);
+        }}
+      />
+      <Text style={props.checked ? styles.checkedTask : styles.unCheckedTask}>
+        {props.task}
+      </Text>
+      <TouchableOpacity
+        onPress={() => {
+          props.removeTask(props.index);
+        }}
+      >
+        <FontAwesomeIcon icon={faTrash} style={styles.icon} size={28} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  item: {
+    flex: 1,
+    flexDirection: "row",
+    height: "auto",
+    marginVertical: "1%",
+    paddingVertical: "1%",
+    backgroundColor: "gray",
+    borderRadius: 20,
+  },
+  checkbox: {
+    width: "7%",
+    height: "45%",
+    color: "#000",
+    marginHorizontal: "1%",
+    marginVertical: "4%",
+  },
+  unCheckedTask: {
+    flex: 3,
+    color: "#000",
+    fontSize: 20,
+    paddingHorizontal: "3%",
+    paddingVertical: "4%",
+  },
+  checkedTask: {
+    flex: 3,
+    color: "#b3b3ba",
+    fontSize: 20,
+    paddingHorizontal: "3%",
+    paddingVertical: "4%",
+    textDecorationLine: "line-through",
+  },
+  icon: {
+    flex: 1,
+    color: "black",
+    marginVertical: "55%",
+    marginHorizontal: "0.5%",
+  },
+});
